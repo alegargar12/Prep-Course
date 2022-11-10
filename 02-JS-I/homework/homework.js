a38992678a9340a6ab0a7cfa9734f4fa070cb6f5
alegargar12 <alegargar12@gmail.com>
2022-11-10 00:27:22 -0600

    cambio ale

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = "primero";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
@@ -10,8 +10,6 @@ const nuevoNum = null;
 const nuevoBool = null;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
-
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
 
